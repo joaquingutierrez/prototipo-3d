382c83f3087c1f77cd99ed6e688981abe7131e2e
joaquingutierrez <joaquinguty@gmail.com>
2022-09-12 12:53:42 -0300

    Componente ItemCount movido a ItemListContainer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar'
 import ItemListContainer from './components/ItemListContainer'
-import ItemCount from './components/ItemCount';
-
-const onAdd= () => {
-  console.log("Agregado al carrito");
-}
 
 function App() {
   return (
     <>
       <NavBar />
       <ItemListContainer greeting="Bienvenido a nuestra tienda"/>
-      <ItemCount stock={8} initial={1} onAdd={onAdd}/>
     </>
   );
 }
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import ItemCount from "./ItemCount";
+
+const onAdd= () => {
+    console.log("Agregado al carrito");
+}
 
 const ItemListContainer = ({greeting}) => {
     return (
         <div>
             <h3>{greeting}</h3>
+            <ItemCount stock={8} initial={1} onAdd={onAdd}/>
         </div>
     )
 }
