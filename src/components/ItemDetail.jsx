58772175269ed8175f2971ee619ff5f0cf127a56
joaquingutierrez <joaquinguty@gmail.com>
2022-10-21 12:06:19 -0300

    Si ingreso un /item/id y este id no existe, ahora muestra el mensaje apropiedo

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -6,29 +6,35 @@ import { Button } from "react-bootstrap";
 import { CartContext } from "../context/CartContext";
 
 
-const ItemDetail = ({ product, id }) => {
-    
+const ItemDetail = ({ product }) => {
+
     const { name, image, description, stock, price } = product
     const [checkout, setCheckout] = useState(false)
     const { addItem, addDuplicateItem, isInCart } = useContext(CartContext)
-    
 
-    const onAdd= (contador) => {
+
+    const onAdd = (contador) => {
         (isInCart(product.id) + 1) ? addDuplicateItem(isInCart(product.id), contador) : addItem(product, contador)
         setCheckout(true)
     }
 
-
     return (
-        <div className="containerItemDetail">
-                <img className="image" src={image} alt={name} />
-            <p className="description">{description}</p>
-            <div className="buy">
-                <h2>{name}</h2>
-                <h4>${price}</h4>
-                {checkout ? <Button variant="success"><Link to="/cart" className="style-link">Finalizar compra</Link></Button> : (<ItemCount stock={stock} initial={1} onAdd={onAdd} />)}
-            </div>
-        </div>
+        <>
+            {name ? (
+                <div className="containerItemDetail">
+                    <img className="image" src={image} alt={name} />
+                    <p className="description">{description}</p>
+                    <div className="buy">
+                        <h2>{name}</h2>
+                        <h4>${price}</h4>
+                        {checkout ? <Button variant="success"><Link to="/cart" className="style-link">Finalizar compra</Link></Button> : (<ItemCount stock={stock} initial={1} onAdd={onAdd} />)}
+                    </div>
+                </div>
+
+            ) : (
+                <h2 className="center">El producto solicidato no existe</h2>
+            )}
+        </>
     )
 }
 
--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -1,4 +1,4 @@
-import React, {useContext} from "react"
+import React, { useContext } from "react"
 import ItemDetail from './ItemDetail'
 import { useState, useEffect } from "react";
 import Spinner from 'react-bootstrap/Spinner'
@@ -23,26 +23,32 @@ const ItemDetailContainer = () => {
     useEffect(() => {
 
         const q = doc(db, 'products', id)
-        
+
         getDoc(q)
-            .then((data) => {       
-                setProduct(
-                    {
-                        ...data.data(),
-                        id: data.id,
-                        stock: stockLocalControl(cart, data.data(), data.id) >= 0 ? stockLocalControl(cart, data.data(), data.id) : data.data().stock
-                    }
+            .then((data) => {
+                console.log(data.data())
+                data.data() ? (
+                    setProduct(
+                        {
+                            ...data.data(),
+                            id: data.id,
+                            stock: stockLocalControl(cart, data.data(), data.id) >= 0 ? stockLocalControl(cart, data.data(), data.id) : data.data().stock
+                        }
                     )
-                })
-                .finally(()=>{
-                    setLoading(false)
+                ) : (
+                    setProduct({})
+                )
+
             })
-    }, [id, cart, product, stockLocalControl])
+            .finally(() => {
+                setLoading(false)
+            })
+    }, [id, cart, stockLocalControl])
 
 
     return (
         <div className="containerDetail">
-            {loading ? <div className="center"><Spinner animation="border" role="status" /></div> : <ItemDetail product={product} id={id} />}
+            {loading ? <div className="center"><Spinner animation="border" role="status" /></div> : <ItemDetail product={product} />}
         </div>
     )
 
